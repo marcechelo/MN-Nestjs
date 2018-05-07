4b627198d21d0d21a0a4281d4877f7e98443202e
marcelo nieto <marcechelo25@hotmail.com>
2018-05-07 11:07:21 -0500

    Controllers

--- Ejemplo/src/usuario.controller.ts
+++ Ejemplo/src/usuario.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Req} from "@nestjs/common";
+import {Controller, Get, HttpCode, Post, Req} from "@nestjs/common";
 import {Res} from "@nestjs/common/utils/decorators/route-params.decorator";
 
 @Controller('Usuario')
@@ -9,8 +9,28 @@ export class UsuarioController{
         edad: 23
     };
 
+    usuarios = [];
+
+    @HttpCode(202)
     @Get('mostrar')
-    mostrarUsuario(@Req() request,@Res() response){
-        return response.send(this.usuario);
+    mostrarUsuario(){
+        return this.usuario;
+    }
+
+    @Get('mostrarExpress')
+    mostrarUsuarioExpress(@Req() request,@Res() response){
+        return response.status(200).send(this.usuarios);
+    }
+
+    @Post('crearUsuario')
+    crearUsuario(@Req() request,@Res() response){
+        const nuevoUsuario ={
+            nombre: request.query.nombre,
+            apellido: request.query.apellido,
+            edad: request.query.edad
+        };
+        this.usuarios.push(nuevoUsuario);
+
+        return response.status(201).send(nuevoUsuario);
     }
 }
