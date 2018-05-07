e0e27fbc3ad362676eed3f18ca5c9fa4b949fc5d
marcelo nieto <marcechelo25@hotmail.com>
2018-05-07 10:19:46 -0500

    Controller

--- Ejemplo/src/app.controller.ts
+++ Ejemplo/src/app.controller.ts
@@ -1,9 +1,35 @@
-import { Get, Controller } from '@nestjs/common';
+import {Get, Controller, Req} from '@nestjs/common';
+import {Res} from "@nestjs/common/utils/decorators/route-params.decorator";
 
-@Controller()
+const fs = require("fs");
+
+
+@Controller() //decorators
 export class AppController {
-  @Get()
-  root(): string {
-    return 'Hola Mundo';
-  }
+    @Get()
+    root(@Req() request, @Res() response){
+
+        console.log('1 Inicio');
+        let contenidoHtml = '';
+        console.log('2 Contenido Html', contenidoHtml);
+        fs.readFile(__dirname + '/html/Index.html', 'utf8', (error, contenidoDelArchivo) => {
+            console.log('3 Respondio');
+            const nombre = 'Marcelo';
+            if (error) {
+                console.log('4 Error ', error);
+                console.log('5 contenido Html', contenidoHtml);
+                console.log('6 Termino');
+                return response.send('Error');
+            } else {
+                contenidoHtml = contenidoDelArchivo;
+                contenidoHtml = contenidoHtml.replace('{{nombre}}',nombre);
+                console.log('4 Contenido Html', contenidoHtml);
+                console.log('5 contenido Html', contenidoHtml);
+                console.log('6 Termino');
+                return response.send(contenidoHtml);
+            }
+        });
+        console.log('5 contenido Html', contenidoHtml);
+        console.log('6 Termino');
+    }
 }
