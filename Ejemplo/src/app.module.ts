f1f9bd8d638154d3f0103ffaaae9a2335e6cdfa2
marcelo nieto <marcechelo25@hotmail.com>
2018-05-09 11:58:24 -0500

    comandos

--- Ejemplo/src/app.module.ts
+++ Ejemplo/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import {UsuarioController} from "./usuario.controller";
+import {ParametrosController} from "./parametros.controller";
 
 @Module({
   imports: [], //otros modulos
-  controllers: [AppController,UsuarioController],
+  controllers: [AppController,UsuarioController,ParametrosController],
   components: [],
 })
 export class AppModule {}
